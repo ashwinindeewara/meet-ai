5dd2db1f4a7def13ae367dceed0c37b31dab0119
Ashwin Indeewara <ashwinindeewara@gmail.com>
2025-09-30 18:15:39 +0530

    authentication-ui

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db"; // your drizzle instance
-import * as schema from "@/db/schema";
+import { db } from "@/db";
 
+import * as schema from "@/db/schema"
 export const auth = betterAuth({
-
-        emailAndPassword:{
-            enabled:true,
-        },
+    emailAndPassword: { // lowercase 'a'
+        enabled: true,
+    },
     database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-        schema:{
+        provider: "pg",
+        schema: {
             ...schema,
         },
     }),
